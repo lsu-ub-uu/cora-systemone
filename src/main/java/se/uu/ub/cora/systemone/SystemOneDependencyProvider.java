079a7cd5d56f4ddfa8299312280f67f52693d93e
olov <olov.mckie@ub.uu.se>
2016-03-23 11:55:40 +0100

    removed earlier removed metadataTypeCollectionVariableChildItem

--- src/main/java/se/uu/ub/cora/systemone/SystemOneDependencyProvider.java
+++ src/main/java/se/uu/ub/cora/systemone/SystemOneDependencyProvider.java
@@ -19,6 +19,9 @@
 
 package se.uu.ub.cora.systemone;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import se.uu.ub.cora.beefeater.Authorizator;
 import se.uu.ub.cora.beefeater.AuthorizatorImp;
 import se.uu.ub.cora.bookkeeper.data.DataAtomic;
@@ -36,9 +39,6 @@ import se.uu.ub.cora.spider.record.storage.RecordStorage;
 import se.uu.ub.cora.spider.record.storage.RecordStorageInMemory;
 import se.uu.ub.cora.spider.record.storage.TimeStampIdGenerator;
 import se.uu.ub.cora.systemone.record.RecordPermissionKeyCalculator;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * SystemOneDependencyProvider wires up the system for use in "production", as
@@ -178,8 +178,10 @@ public class SystemOneDependencyProvider implements SpiderDependencyProvider {
 		dummyRR.addChild(DataAtomic.withNameInDataAndValue(NAME_IN_DATA, "dummyRelation"));
 		dummyRR.addChild(DataAtomic.withNameInDataAndValue(TEXT_ID, id + "Text"));
 		dummyRR.addChild(DataAtomic.withNameInDataAndValue(DEF_TEXT_ID, id + DEF_TEXT));
-		dummyRR.addChild(DataAtomic.withNameInDataAndValue("refRecordLinkId", id + "refRecordLinkId"));
-		dummyRR.addChild(DataAtomic.withNameInDataAndValue("refMetadataGroupId", id + "refMetadataGroupId"));
+		dummyRR.addChild(
+				DataAtomic.withNameInDataAndValue("refRecordLinkId", id + "refRecordLinkId"));
+		dummyRR.addChild(
+				DataAtomic.withNameInDataAndValue("refMetadataGroupId", id + "refMetadataGroupId"));
 		DataGroup childReferences = DataGroup.withNameInData(CHILD_REFERENCES);
 		dummyRR.addChild(childReferences);
 
@@ -479,6 +481,7 @@ public class SystemOneDependencyProvider implements SpiderDependencyProvider {
 		createMetadataTypeCollectionItem("itemCollection");
 		createMetadataTypeCollectionItem("collectionItem");
 		createMetadataTypeCollectionItem("collectionVariable");
+		createMetadataTypeCollectionItem("recordRelation");
 	}
 
 	private void createMetadataTypeCollectionItem(String id) {
@@ -514,8 +517,8 @@ public class SystemOneDependencyProvider implements SpiderDependencyProvider {
 				DataAtomic.withNameInDataAndValue("ref", "metadataTypeCollectionItemItem"));
 		collectionItemReferences.addChild(
 				DataAtomic.withNameInDataAndValue("ref", "metadataTypeCollectionVariableItem"));
-		collectionItemReferences.addChild(DataAtomic.withNameInDataAndValue("ref",
-				"metadataTypeCollectionVariableChildItem"));
+		collectionItemReferences.addChild(
+				DataAtomic.withNameInDataAndValue("ref", "metadataTypeRecordRelationItem"));
 
 		recordStorage.create(MetadataTypes.ITEMCOLLECTION.type, METADATA_TYPE_COLLECTION,
 				dataGroup2, emptyLinkList);
