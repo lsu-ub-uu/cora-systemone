0d497881bfb9d1f8e7187fbf5d288f6339439ad9
olovm <olov.mckie@ub.uu.se>
2019-06-05 13:55:26 +0000

    Revert "CORA-916 work for module startup for the rest"
    This reverts commit 8ade1640f9b28d94aa284bc42b9504263f46ca23.

--- src/main/java/se/uu/ub/cora/systemone/SystemOneDependencyProvider.java
+++ src/main/java/se/uu/ub/cora/systemone/SystemOneDependencyProvider.java
@@ -20,8 +20,17 @@
 package se.uu.ub.cora.systemone;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Map;
 
+import se.uu.ub.cora.beefeater.AuthorizatorImp;
+import se.uu.ub.cora.bookkeeper.linkcollector.DataRecordLinkCollector;
+import se.uu.ub.cora.bookkeeper.linkcollector.DataRecordLinkCollectorImp;
+import se.uu.ub.cora.bookkeeper.storage.MetadataStorage;
+import se.uu.ub.cora.bookkeeper.termcollector.DataGroupTermCollector;
+import se.uu.ub.cora.bookkeeper.termcollector.DataGroupTermCollectorImp;
+import se.uu.ub.cora.bookkeeper.validator.DataValidator;
+import se.uu.ub.cora.bookkeeper.validator.DataValidatorImp;
 import se.uu.ub.cora.gatekeeperclient.authentication.AuthenticatorImp;
 import se.uu.ub.cora.httphandler.HttpHandlerFactory;
 import se.uu.ub.cora.httphandler.HttpHandlerFactoryImp;
@@ -31,11 +40,21 @@ import se.uu.ub.cora.solr.SolrClientProviderImp;
 import se.uu.ub.cora.solrindex.SolrRecordIndexer;
 import se.uu.ub.cora.solrsearch.SolrRecordSearch;
 import se.uu.ub.cora.spider.authentication.Authenticator;
+import se.uu.ub.cora.spider.authorization.BasePermissionRuleCalculator;
+import se.uu.ub.cora.spider.authorization.PermissionRuleCalculator;
+import se.uu.ub.cora.spider.authorization.SpiderAuthorizator;
+import se.uu.ub.cora.spider.authorization.SpiderAuthorizatorImp;
 import se.uu.ub.cora.spider.dependency.SpiderDependencyProvider;
 import se.uu.ub.cora.spider.extended.ExtendedFunctionalityProvider;
 import se.uu.ub.cora.spider.record.RecordSearch;
+import se.uu.ub.cora.spider.record.storage.RecordIdGenerator;
+import se.uu.ub.cora.spider.record.storage.RecordStorage;
+import se.uu.ub.cora.spider.record.storage.TimeStampIdGenerator;
+import se.uu.ub.cora.spider.role.RulesProvider;
+import se.uu.ub.cora.spider.role.RulesProviderImp;
 import se.uu.ub.cora.spider.search.RecordIndexer;
-import se.uu.ub.cora.storage.RecordStorage;
+import se.uu.ub.cora.spider.stream.storage.StreamStorage;
+import se.uu.ub.cora.storage.StreamStorageOnDisk;
 
 /**
  * SystemOneDependencyProvider wires up the system for use in "production", as this is in SystemOne
@@ -46,11 +65,16 @@ import se.uu.ub.cora.storage.RecordStorage;
 public class SystemOneDependencyProvider extends SpiderDependencyProvider {
 
 	private RecordStorage recordStorage;
+	private MetadataStorage metadataStorage;
+	private RecordIdGenerator idGenerator;
+	private StreamStorage streamStorage;
 	private String gatekeeperUrl;
 	private String solrUrl;
 	private SolrRecordIndexer solrRecordIndexer;
 	private SolrClientProviderImp solrClientProvider;
 	private SearchStorage searchStorage;
+	private String basePath;
+	private String storageOnDiskClassName;
 
 	public SystemOneDependencyProvider(Map<String, String> initInfo) {
 		super(initInfo);
@@ -59,27 +83,95 @@ public class SystemOneDependencyProvider extends SpiderDependencyProvider {
 	@Override
 	protected void readInitInfo() {
 		tryToSetGatekeeperUrl();
+		basePath = tryToGetStorageOnDiskBasePath();
+		storageOnDiskClassName = tryToGetStorageOnDiskClassName();
 		tryToSetSolrUrl();
 	}
 
-	private void tryToSetGatekeeperUrl() {
-		ensureKeyExistsInInitInfo("gatekeeperURL");
-		gatekeeperUrl = initInfo.get("gatekeeperURL");
+	private String tryToGetStorageOnDiskClassName() {
+		throwErrorIfMissingKeyIsMissingFromInitInfo("storageOnDiskClassName");
+		return initInfo.get("storageOnDiskClassName");
 	}
 
-	private void tryToSetSolrUrl() {
-		ensureKeyExistsInInitInfo("solrURL");
-		solrUrl = initInfo.get("solrURL");
+	private void throwErrorIfMissingKeyIsMissingFromInitInfo(String key) {
+		if (!initInfo.containsKey(key)) {
+			throw new RuntimeException("InitInfo must contain " + key);
+		}
 	}
 
 	@Override
 	protected void tryToInitialize() throws NoSuchMethodException, ClassNotFoundException,
 			IllegalAccessException, InvocationTargetException {
+		recordStorage = tryToCreateRecordStorage(basePath);
 
+		metadataStorage = (MetadataStorage) recordStorage;
+		idGenerator = new TimeStampIdGenerator();
+		streamStorage = StreamStorageOnDisk.usingBasePath(basePath + "streams/");
 		solrClientProvider = SolrClientProviderImp.usingBaseUrl(solrUrl);
 		solrRecordIndexer = SolrRecordIndexer
 				.createSolrRecordIndexerUsingSolrClientProvider(solrClientProvider);
 		searchStorage = (SearchStorage) recordStorage;
+	}
+
+	private RecordStorage tryToCreateRecordStorage(String basePath) throws NoSuchMethodException,
+			ClassNotFoundException, IllegalAccessException, InvocationTargetException {
+		Class<?>[] cArg = new Class[1];
+		cArg[0] = String.class;
+		Method constructor = Class.forName(storageOnDiskClassName)
+				.getMethod("createRecordStorageOnDiskWithBasePath", cArg);
+		return (RecordStorage) constructor.invoke(null, basePath);
+	}
+
+	private void tryToSetGatekeeperUrl() {
+		throwErrorIfMissingKeyIsMissingFromInitInfo("gatekeeperURL");
+		gatekeeperUrl = initInfo.get("gatekeeperURL");
+	}
+
+	private String tryToGetStorageOnDiskBasePath() {
+		throwErrorIfMissingKeyIsMissingFromInitInfo("storageOnDiskBasePath");
+		return initInfo.get("storageOnDiskBasePath");
+	}
+
+	private void tryToSetSolrUrl() {
+		throwErrorIfMissingKeyIsMissingFromInitInfo("solrURL");
+		solrUrl = initInfo.get("solrURL");
+	}
+
+	@Override
+	public SpiderAuthorizator getSpiderAuthorizator() {
+		RulesProvider rulesProvider = new RulesProviderImp(recordStorage);
+		return SpiderAuthorizatorImp.usingSpiderDependencyProviderAndAuthorizatorAndRulesProvider(
+				this, new AuthorizatorImp(), rulesProvider);
+	}
+
+	@Override
+	public RecordStorage getRecordStorage() {
+		return recordStorage;
+	}
+
+	@Override
+	public RecordIdGenerator getIdGenerator() {
+		return idGenerator;
+	}
+
+	@Override
+	public PermissionRuleCalculator getPermissionRuleCalculator() {
+		return new BasePermissionRuleCalculator();
+	}
+
+	@Override
+	public DataValidator getDataValidator() {
+		return new DataValidatorImp(metadataStorage);
+	}
+
+	@Override
+	public DataRecordLinkCollector getDataRecordLinkCollector() {
+		return new DataRecordLinkCollectorImp(metadataStorage);
+	}
+
+	@Override
+	public StreamStorage getStreamStorage() {
+		return streamStorage;
 	}
 
 	@Override
@@ -98,6 +190,11 @@ public class SystemOneDependencyProvider extends SpiderDependencyProvider {
 	public RecordSearch getRecordSearch() {
 		return SolrRecordSearch.createSolrRecordSearchUsingSolrClientProviderAndSearchStorage(
 				solrClientProvider, searchStorage);
+	}
+
+	@Override
+	public DataGroupTermCollector getDataGroupTermCollector() {
+		return new DataGroupTermCollectorImp(metadataStorage);
 	}
 
 	@Override
---
+++ src/test/java/se/uu/ub/cora/systemone/RecordStorageSpy.java
@@ -0,0 +1,138 @@
+package se.uu.ub.cora.systemone;
+
+import java.util.Collection;
+
+import se.uu.ub.cora.bookkeeper.data.DataGroup;
+import se.uu.ub.cora.bookkeeper.storage.MetadataStorage;
+import se.uu.ub.cora.searchstorage.SearchStorage;
+import se.uu.ub.cora.spider.data.SpiderReadResult;
+import se.uu.ub.cora.spider.record.storage.RecordStorage;
+
+public class RecordStorageSpy implements RecordStorage, MetadataStorage, SearchStorage {
+
+	private String basePath;
+
+	public static RecordStorageSpy createRecordStorageOnDiskWithBasePath(String basePath) {
+		return new RecordStorageSpy(basePath);
+	}
+
+	private RecordStorageSpy(String basePath) {
+		this.basePath = basePath;
+	}
+
+	@Override
+	public DataGroup read(String type, String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void create(String type, String id, DataGroup record, DataGroup collectedTerms,
+			DataGroup linkList, String dataDivider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteByTypeAndId(String type, String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean linksExistForRecord(String type, String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void update(String type, String id, DataGroup record, DataGroup collectedTerms,
+			DataGroup linkList, String dataDivider) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public SpiderReadResult readList(String type, DataGroup filter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SpiderReadResult readAbstractList(String type, DataGroup filter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataGroup readLinkList(String type, String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<DataGroup> generateLinkCollectionPointingToRecord(String type, String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean recordsExistForRecordType(String type) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean recordExistsForAbstractOrImplementingRecordTypeAndRecordId(String type,
+			String id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Collection<DataGroup> getMetadataElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<DataGroup> getPresentationElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<DataGroup> getTexts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<DataGroup> getRecordTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<DataGroup> getCollectTerms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataGroup getSearchTerm(String searchTermId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DataGroup getCollectIndexTerm(String collectIndexTermId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getBasePath() {
+		return basePath;
+	}
+
+}
